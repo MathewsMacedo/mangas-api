20e259d9adf30dbc1436abc36b278e08544c03ae
Matheus Macedo <matheus.smacedo@hotmail.com>
2020-06-11 08:15:30 -0300

    Folder Structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :public do 
+      namespace :v1 do 
+        namespace :manga do 
+          
+
+
+        end
+      end
+    end
+  end
 end
